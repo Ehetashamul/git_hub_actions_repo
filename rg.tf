01b766984a3292164f4e477977b2ff289e24b778
Ehetashamul Haque <hehetashamul@gmail.com>
2026-08-25 04:42:57 +0530

    Create resource groups for dev, test, and prod
    Added multiple Azure resource groups for development, testing, and production environments.

--- rg.tf
+++ rg.tf
@@ -1,4 +1,18 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "lab12"
+# Resource Group 1
+resource "azurerm_resource_group" "rg_dev" {
+  name     = "rg-tpg-dev"
   location = "Central India"
 }
+
+# Resource Group 2
+resource "azurerm_resource_group" "rg_test" {
+  name     = "rg-tpg-test"
+  location = "Central India"
+}
+
+# Resource Group 3
+resource "azurerm_resource_group" "rg_prod" {
+  name     = "rg-tpg-prod"
+  location = "Central India"
+}
+
