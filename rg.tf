2716a8822a6b7ba2a4426dac4805b2dd30dd5040
Ehetashamul Haque <hehetashamul@gmail.com>
2026-08-26 04:29:20 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,23 +1,5 @@
-# Resource Group 1
-resource "azurerm_resource_group" "rg_dev" {
-  name     = "rg-tpg-dev"
-  location = "Central India"
-}
-
-# Resource Group 2
-resource "azurerm_resource_group" "rg_test" {
-  name     = "rg-tpg-test"
-  location = "Central India"
-}
-
-# Resource Group 3
-resource "azurerm_resource_group" "rg_prod" {
-  name     = "rg-tpg-prod"
-  location = "Central India"
-}
-
-# Resource Group 4 on self hosted
+# Resource Group 1 on self hosted
 resource "azurerm_resource_group" "rg_tpg" {
-  name     = "rg-tpg"
+  name     = "rg-tpg1"
   location = "Central India"
 }
